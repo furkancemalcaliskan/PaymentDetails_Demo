799680476b669d1d69b4a0b20daca820497c5685
Furkan Cemal Çalışkan <furkanc.caliskan@icloud.com>
2023-12-05 14:37:48 +0300

    Created PaymentAPI and PaymentApp

---
+++ PaymentAPI/PaymentAPI/Context/PaymentDetailContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentAPI.Models;
+
+namespace PaymentAPI.Context
+{
+	public class PaymentDetailContext : DbContext
+	{
+        public PaymentDetailContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
